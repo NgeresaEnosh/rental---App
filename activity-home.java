6dac8e652c6704dd4db1c94903acb962fd49eeb4
NGERESA ENOSH <134959771+NgeresaEnosh@users.noreply.github.com>
2024-04-17 16:54:02 +0300

    activity-home.java

---
+++ activity-home.java
@@ -0,0 +1,14 @@
+package com.example.tester;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_home);
+    }
+}
