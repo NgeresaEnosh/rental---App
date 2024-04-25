c5b0ec291eab843e6a6143bd2f295479e08c0f08
NGERESA ENOSH <134959771+NgeresaEnosh@users.noreply.github.com>
2024-04-25 20:49:51 +0300

    repair.java

--- repair.java
+++ repair.java
@@ -1,38 +1,35 @@
-package com.example.login;
+package com.example.rental;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class RestActivity2 extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class RepairActivity extends AppCompatActivity {
 
     private Button btnElectricity;
     private Button btnWater;
     private Button btnHouseRenovation;
     private CardView descriptionCardView;
     private Button btnSave;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rest2);
+        setContentView(R.layout.activity_repair);
         initializeViews();
         setButtonListeners();
-        EdgeToEdge.enable(this);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setBottomNavigation();
     }
 
     private void initializeViews() {
@@ -41,6 +38,7 @@ public class RestActivity2 extends AppCompatActivity {
         btnHouseRenovation = findViewById(R.id.btnHouseRenovation);
         descriptionCardView = findViewById(R.id.descriptionCardView);
         btnSave = findViewById(R.id.btnSave);
+        bottomNavigationView = findViewById(R.id.bottomnavigator);
     }
 
     private void setButtonListeners() {
@@ -93,5 +91,30 @@ public class RestActivity2 extends AppCompatActivity {
     // Method to show a toast message
     private void showToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void setBottomNavigation() {
+        bottomNavigationView.setSelectedItemId(R.id.search);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.profile) {
+                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                } else if (itemId == R.id.search) {
+                    // No need to start RepairActivity again, already in RepairActivity
+                    return true;
+                } else if (itemId == R.id.repair) {
+                    // Already in RepairActivity, no need to do anything
+                    return true;
+                } else {
+                    Toast.makeText(RepairActivity.this, "Invalid navigation item selected!", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
     }
 }
